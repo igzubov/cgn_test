033d89c7b9fc1f8391adfd892350e911cf642278
Igor Zubov <igorzub93@gmail.com>
2019-03-26 22:21:49 +0300

    add main args

--- main.cpp
+++ main.cpp
@@ -11,28 +11,30 @@
 
 class Application {
 public:
-    Application(const std::string &carName, const std::string &channelName = "b0RemoteApi") {
+    Application(const std::string pointsFileName, const std::string carName = "nakedAckermannSteeringCar",
+                const std::string channelName = "b0RemoteApi") {
         _client = new b0RemoteApi("b0RemoteApi_c++Client", channelName.c_str());
         _robot = new AckermannCar(_client, carName, 2);
+        _pf = new PathFollower(pointsFileName);
     }
 
     ~Application() {
+        delete _pf;
         delete _client;
         delete _robot;
     }
 
     void start() {
         TrajVisualizer travis(_client);
-        PathFollower pf("points.txt");
         // to get start position
         _client->simxSpinOnce();
         std::vector<float> currPos = _robot->getPosition();
         std::vector<float> prevPos = currPos;
         // draw trajectory from points
-        travis.drawTargetTrajectory(currPos, pf.getPoints());
-        while (!pf._achievedPoint | !pf.isEmpty()) {
+        travis.drawTargetTrajectory(currPos, _pf->getPoints());
+        while (!_pf->_achievedPoint | !_pf->isEmpty()) {
             currPos = _robot->getPosition();
-            double deltaAngle = pf.getDeltaAngle(_robot->getYaw(), currPos);
+            double deltaAngle = _pf->getDeltaAngle(_robot->getYaw(), currPos);
             travis.drawCurrTrajectory(prevPos, currPos);
             prevPos = currPos;
 
@@ -49,12 +51,18 @@ public:
 private:
     b0RemoteApi *_client;
     DrivableRobot *_robot;
+    PathFollower *_pf;
 };
 
-int main() {
-    Application app("nakedAckermannSteeringCar");
-    app.start();
-
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        std::string fileName(argv[1]);
+        Application app(fileName);
+        app.start();
+        std::cout << "Hello, World!" << std::endl;
+    }
+    else {
+        std::cout << "No file with points was specified, exiting.." << std:: endl;
+    }
     return 0;
 }
