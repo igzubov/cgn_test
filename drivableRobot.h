efbe4d33a213c163d9f8fe704adccb395c364587
Igor Zubov <igorzub93@gmail.com>
2019-03-26 22:03:50 +0300

    add virtual destructor

--- drivableRobot.h
+++ drivableRobot.h
@@ -1,25 +1,23 @@
 #include <boost/geometry/geometries/point_xy.hpp>
 #include "b0RemoteApi.h"
 
-using namespace boost::geometry;
-using Point2D = model::point<double, 2, cs::cartesian>;
-
-
 class DrivableRobot {
 public:
-    DrivableRobot(b0RemoteApi *client, const std::string &name) {
+    DrivableRobot(b0RemoteApi *client, const std::string name, const unsigned long dim) : _position(dim) {
         _client = client;
         auto answ = client->simxGetObjectHandle(name.c_str(), _client->simxServiceCall());
         _robotHandle = b0RemoteApi::readInt(answ, 1);
     };
-    Point2D getPosition() {return _position;};
+
+    virtual ~DrivableRobot() = {};
+    std::vector<float> getPosition() {return _position;};
     double getYaw() {return _yaw * 180 / M_PI;};
     // in degrees
     virtual void setSteeringAngle(double angle) = 0;
     virtual void setSpeed(float speed) = 0;
 protected:
     b0RemoteApi *_client;
-    int _robotHandle;
-    Point2D _position;
-    double _yaw;
+    int _robotHandle = 0;
+    std::vector<float> _position;
+    double _yaw = 0;
 };
